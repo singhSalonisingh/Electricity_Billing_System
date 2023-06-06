1b4bd87f0c0a1a54ba4853100cc4caa14a49d1b5
Saloni singh <salonisingh15jun@gmail.com>
2023-06-06 02:05:26 -0500

    final commit

---
+++ src/Electricity/Conn.java
@@ -0,0 +1,16 @@
+package Electricity;
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "Surabhi@9oct");
+            s = c.createStatement();
+    }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
